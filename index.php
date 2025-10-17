ece1d9772e477a6c76e7fec1fad62ee83730ea3b
walid <walidsolid1@gmail.com>
2025-10-17 22:49:57 +0100

    making the main structure and fill this pages (index.php , user_add.php) and some libraries like (bootstrap + feather icons)

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./libs/bootstrap/css/bootstrap.min.css">
+    <title>Document</title>
+</head>
+<body>
+
+    <div class="container">
+        <div class="wrapper p-5 m-5">
+            <div class="d-flex justify-content-between p-2">
+                <h2>All users</h2>
+                <div><a href="add_user.php"><i data-feather="user-plus"></i></a></div>
+            </div>
+
+        </div>
+    </div>
+    
+    
+
+    <script src="./libs/bootstrap/js/bootstrap.min.js"></script>
+    <script src="./libs/feather_icons.js"></script>
+    <script>feather.replace();</script>
+
+</body>
+</html>
---
+++ libs/DB/readme.txt
@@ -0,0 +1,3 @@
+
+
+## You can make import to this descriptive DB in your "localhost/phpmyadmin" to make your DB automatically ##
