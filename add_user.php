6e07c848df750aa7431bf6cead0b97fd9c45e00b
walid <walidsolid1@gmail.com>
2025-10-18 14:01:59 +0100

    add (jqery & toastr) libs and test toastr

--- add_user.php
+++ add_user.php
@@ -4,33 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./libs/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./libs/toast/toast.css">
     <title>Document</title>
 </head>
 <body>
-
-    <?php
-
-        include ('db.php');
-
-        if (isset($_POST['save'])) {
-            
-            $name = $_POST['name'];
-            $email = $_POST['email'];
-            $mobile = $_POST['mobile'];
-            $password = $_POST['password'];
-        };
-        $add_sql = "INSERT INTO 'users'('name','email','password','mobile')  VALUES ('$name','$email','$password','$mobile')";
-
-        $result = mysqli_query($con,$add_sql);
-
-        if(!$result){
-            die(mysqli_error($con));
-        }else{
-            echo "Successfully connection" ;
-        }
-
-    ?>
-
     
     <div class="container">
         <div class="wrapper p-5 m-5">
@@ -66,8 +43,10 @@
     </div>
     
     
+    <script src="./libs/jq.js"></script>
     <script src="./libs/bootstrap/js/bootstrap.min.js"></script>
     <script src="./libs/feather_icons.js"></script>
+    <script src="./libs/toast/toast.js"></script>
     <script>feather.replace();</script>
 
 </body>
--- index.php
+++ index.php
@@ -1,12 +1,39 @@
+    <?php
+        include_once ('db.php');
+
+        $action = false ;
+
+        if(isset($_POST['save'])) {
+            
+            $name = $_POST['name'];
+            $email = $_POST['email'];
+            $mobile = $_POST['mobile'];
+            $password = $_POST['password'];
+        };
+        $add_sql = "INSERT INTO 'users'('name','email','password','mobile')  VALUES ('$name','$email','$password','$mobile')";
+
+        $result = mysqli_query($con,$add_sql);
+
+        if(!$result){
+            die(mysqli_error($con));
+        }else{
+            echo "Successfully connection" ;
+        };
+
+    ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./libs/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./libs/toast/toast.css">
     <title>Document</title>
 </head>
 <body>
+
 
     <div class="container">
         <div class="wrapper p-5 m-5">
@@ -20,9 +47,24 @@
     
     
 
+    <script src="./libs/jq.js"></script>
     <script src="./libs/bootstrap/js/bootstrap.min.js"></script>
     <script src="./libs/feather_icons.js"></script>
-    <script>feather.replace();</script>
+    <script src="./libs/toast/toast.js"></script>
+    <script src="./libs/toast/main.js"></script>
+    <script>
+        feather.replace();
+        <?php
+        if($action != false){
+            if($action='add'){?>
+                <script>
+                    show_add()
+                </script>
+        <?php  
+            }
+        }
+        ?>
+    </script>
 
 </body>
 </html>
